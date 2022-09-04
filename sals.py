005c9fae15a5565e7ac43e3ce0393f44835a92d6
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2022-09-04 15:58:10 -0500

    v0.1

--- sals.py
+++ sals.py
@@ -1,3 +1,12 @@
 weight = 31
 
 # Ground Shipping
+
+if weight <= 2:
+  print("$1.50 + $20 flat charge = $21.50"
+elif weight > 2 or weight <= 6:
+  print("$3.00 + $20 flat charge = $23.00")
+elif weight > 6 or weight <= 10:
+  print("$4.00 + $20 flat charge = $24.00")
+else:
+  print("$4.75 + $20 flat charge = $24.75")
